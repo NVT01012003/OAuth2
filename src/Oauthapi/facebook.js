e8bfeb917aead0c1b990325739798ea04239b72b
TrangNv <105917808+NVT01012003@users.noreply.github.com>
2024-01-09 15:49:01 +0700

    update

--- src/Oauthapi/facebook.js
+++ src/Oauthapi/facebook.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const client_id = process.env.FACEBOOK_CLIENT_ID;
-export const redirect_uri = "http://localhost:5050/auth/facebook/callback";
+export const redirect_uri = "http://localhost:3000/auth/facebook/callback";
 const scope = ["email", "public_profile"].join(",");
 
 const stringifiedParams = `client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=code&auth_type=rerequest&display=popup`;
